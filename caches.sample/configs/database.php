6918e908aeeea509e736a1128d62a8d43af5ad06
branthuang <381114687@qq.com>
2017-03-30 17:51:59 +0800

    phpcms框架

---
+++ caches.sample/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'port' => 3306,
+		'database' => 'dynamic_form',
+		'username' => 'root',
+		'password' => '111111',
+		'tablepre' => 'dy_',
+		'charset' => 'utf8',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 1,
+		'autoconnect' => 0
+		),
+);
+
+?>
